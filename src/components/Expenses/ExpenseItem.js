f72a2ac037e9b65561050dfba6205872d22c596c
Elton Lac <lacelton@yahoo.com>
2021-09-16 22:50:23 -0700

    Creating expense inputs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,15 @@
+import { useState } from 'react';
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-
-  let title = props.title;
+  
+  const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    title = "Updated";
+    setTitle("Updated!");
+    console.log(title);
   };
 
   return (
