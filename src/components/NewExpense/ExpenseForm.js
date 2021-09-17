30bb8bf26d86628546f1c599334569059b3fdbd8
Elton Lac <lacelton@yahoo.com>
2021-09-16 22:54:56 -0700

    Created expense form with styling

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -8,6 +8,17 @@ const ExpenseForm = () => {
           <label>Title</label>
           <input type="text" />
         </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
       </div>
     </form>
   );
