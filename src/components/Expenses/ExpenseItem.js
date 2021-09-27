f09fb6c6fc49acfc1b0584531e97011fe6223a86
Elton Lac <lacelton@yahoo.com>
2021-09-27 00:02:44 -0700

    Reformatted all documents

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  
   const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
